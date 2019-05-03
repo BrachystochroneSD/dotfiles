b1a780ebf2a85df8413fe1daceabfd1106df5bc3
Samuel D <samrenfou@hotmail.com>
2019-05-03 15:44:09 +0200

    remove useless files

--- .config/wpg/formats/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f6edd1", "#223322" },
-	[SchemeSel] = { "#f6edd1", "#817D9E" },
-	[SchemeOut] = { "#f6edd1", "#ECD7A0" },
-};
--- .config/wpg/formats/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#223322";
-static const char* selfgcolor   = "#f6edd1";
-static const char* normbgcolor  = "#57A7B8";
-static const char* normfgcolor  = "#f6edd1";
-static const char* urgbgcolor   = "#817D9E";
-static const char* urgfgcolor   = "#f6edd1";
