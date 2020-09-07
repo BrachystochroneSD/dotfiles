61912cf668389243946e830b2c5760a022b7b856
Samuel D <samrenfou@hotmail.com>
2020-09-07 13:27:33 +0200

    fix remote ncmpcpp for musicvize

--- .script/musicvizu/musicvizu.sh
+++ .script/musicvizu/musicvizu.sh
@@ -3,7 +3,7 @@
 exec st 2>/dev/null -n cavaviz -e cava &
 exec st -n album -e ~/.script/musicvizu/albumshow.sh & 2>/dev/null
 if [ -n "$1" ];then
-    st -n ncmpcpp -s playlist -h 192.168.0.103
+    st -n ncmpcppwin -e ncmpcpp -s playlist -h 192.168.0.103
 else
     st -n ncmpcppwin -e ncmpcpp -s playlist
 fi
