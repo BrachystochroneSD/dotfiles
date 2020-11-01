8e1c3e8b8eded2de5ab31b80abf2aafef6e46794
Samuel Dawant <samrenfou@hotmail.com>
2020-11-01 14:38:57 +0100

    add fzf open-with option + xbinding

--- .script/fzf-open.sh
+++ .script/fzf-open.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
 case $1 in
+    --open-with)
+        bin=$(ls /usr/bin | fzf)
+        shit=$(fzf --layout=reverse --height 40%)
+        ;;
     --hidden)
-    shit=$(find . -type f | fzf --layout=reverse --height 40% ) ;;
+        shit=$(find . -type f | fzf --layout=reverse --height 40% )
+    ;;
     *)
-    shit=$(fzf --layout=reverse --height 40%)
+        shit=$(fzf --layout=reverse --height 40%)
     ;;
 esac
+
 if [ -n "$shit" ];then
-    nohup xdg-open "$shit" >/dev/null 2>&1 &
+    if [ -n "$bin" ];then
+        nohup $bin "$shit" >/dev/null 2>&1 &
+    else
+        nohup xdg-open "$shit" >/dev/null 2>&1 &
+    fi
     echo "Opening $shit"
     sleep 1
 fi
