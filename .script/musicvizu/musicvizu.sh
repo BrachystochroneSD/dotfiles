545335e0f2d9ae87380ba82a33622e4e0f3a1e1a
Samuel Dawant <samrenfou@hotmail.com>
2020-12-04 15:28:34 +0100

    add a little delay to fix the focus

--- .script/musicvizu/musicvizu.sh
+++ .script/musicvizu/musicvizu.sh
@@ -2,6 +2,7 @@
 
 exec st 2>/dev/null -n cavaviz -e cava &
 exec st -n album -e ~/.script/musicvizu/albumshow.sh & 2>/dev/null
+sleep 0.1
 if [ -n "$1" ];then
     st -n ncmpcppwin -e ncmpcpp -s playlist -h 192.168.0.103
 else
