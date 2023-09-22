92d603c64991ef06ea8534cd0d0f2cb510715e3d
Samuel D <samueld@mailo.com>
2023-09-23 00:03:07 +0200

    autoinsert and pkgbuild

---
+++ .emacs.d/init.d/auto-insert.el
@@ -0,0 +1,32 @@
+;;; auto-insert.el ---                               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Samuel D
+
+;; Author: Samuel D <samueld@mailo.com>
+;; Keywords: config
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+(auto-insert-mode)
+(setq auto-insert-directory "~/.emacs.d/mytemplates/")
+(setq auto-insert-query nil)
+
+;;; Code:
+
+
+
+(provide 'auto-insert)
+;;; auto-insert.el ends here
