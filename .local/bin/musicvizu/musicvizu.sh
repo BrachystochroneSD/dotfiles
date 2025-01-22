1c5fc4b1ce3b61342747c9fb0e617168986cbdf8
Samuel D <samueld@mailo.com>
2025-01-22 16:18:36 +0100

    add hyprpaer conf

--- .local/bin/musicvizu/musicvizu.sh
+++ .local/bin/musicvizu/musicvizu.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+term=alacritty
+
 winid=$(xprop -root _NET_ACTIVE_WINDOW | cut -d' ' -f5)
 
-exec st 2>/dev/null -t cavaviz -e 'cava' &
-exec st -t album -e ~/.local/bin/musicvizu/albumshow.sh & 2>/dev/null
+exec $term -t cavaviz -e 'cava' &
+exec $term -t album -e ~/.local/bin/musicvizu/albumshow.sh & 2>/dev/null
 sleep 0.1
 wmctrl -i -a "$winid"
 if [ -n "$1" ];then
