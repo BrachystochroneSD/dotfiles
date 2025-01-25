fbbb70f0e76f122576d200e19a1f7ddd296d15f1
Samuel D <samueld@mailo.com>
2025-01-25 11:28:39 +0100

    feh loop for album

--- .local/bin/musicvizu/musicvizu.sh
+++ .local/bin/musicvizu/musicvizu.sh
@@ -3,7 +3,14 @@
 
 TERM=alacritty
 MUSIC_DIR=$(grep music_dir ${HOME}/.config/mpd/mpd.conf | sed 's|.*"\(.*\)/".*|\1|;s/\\//g' | sed "s|~|${HOME}|")
-BACKUP_IMG=${HOME}/.config/wpg/mywalls/owl.png #when no cover img found
+BACKUP_IMG=${HOME}/.config/wpg/mywalls/owl.png
+
+kill_loop() {
+    [ -n "$LOOP_PID" ] && kill "$LOOP_PID"
+    rm /tmp/musicvizu_album
+}
+
+trap "kill_loop" TERM EXIT
 
 get_image() {
     file=$1
@@ -13,32 +20,42 @@ get_image() {
     [ -n "$radio" ] && echo "${HOME}/.local/bin/musicvizu/radio_img/${radio%% *}.png" && return
 
     album_dir="${file%/*}"
-    if [ -n "$album_dir" ] ; then
-        album_dir="$MUSIC_DIR/$album_dir"
-        covers="$(find "$album_dir" -type d -exec find {} -maxdepth 1 -type f -iregex ".*/.*\(${album}\|cover\|folder\|artwork\|front\).*[.]\(jpe?g\|png\|gif\|bmp\)" \; )"
-        nosmall=$(echo "$covers" | grep -v "Small")
-        [ -n "$nosmall" ] && covers="$nosmall"
-        src="$(printf "$covers" | head -n1)"
-        echo $src
-    fi
+    [ -z "$album_dir" ] && return 1
+
+    album_dir="$MUSIC_DIR/$album_dir"
+    covers="$(find "$album_dir" -type d -exec find {} -maxdepth 1 -type f -iregex ".*/.*\(${album}\|cover\|folder\|artwork\|front\).*[.]\(jpe?g\|png\|gif\|bmp\)" \; )"
+    nosmall=$(echo "$covers" | grep -v "Small")
+    [ -n "$nosmall" ] && covers="$nosmall"
+    printf "$covers" | head -n1
 }
 
 get_source_image() {
-    src=""
     album=$(mpc --format %album% current 2>/dev/null)
     file=$(mpc --format %file% current 2>/dev/null)
 
-    if [ -n "$file" ]; then
-        get_image "$file"
-    else
-        echo "$BACKUP_IMG"
-    fi
+    [ -n "$file" ] && image=$(get_image "$file")
+    [ -z "$image" ] && image="$BACKUP_IMG"
+    echo "$image"
 }
 
+check_file() {
+    while true; do
+        sleep 1
+        cp "$(get_source_image)" /tmp/musicvizu_album
+    done
+}
+
+cp "$(get_source_image)" /tmp/musicvizu_album
+
 exec $TERM -t cavaviz -e 'cava' &
-feh -F --title album "$(get_source_image)" &
+check_file &
+LOOP_PID=$!
+
+feh -F --title album "/tmp/musicvizu_album" &
 if [ -n "$1" ]; then
     ncmpcpp -s playlist -h "$1"
 else
     ncmpcpp -s playlist
 fi
+
+kill_loop
